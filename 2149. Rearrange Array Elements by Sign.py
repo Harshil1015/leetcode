a9bcfebc9739e328894bc5a7e85b04588af457bb
Harshil1015 <harshil9225@gmail.com>
2025-06-24 21:18:30 -0400

    update 2149. Rearrange Array Elements by Sign

--- 2149. Rearrange Array Elements by Sign.py
+++ 2149. Rearrange Array Elements by Sign.py
@@ -1,15 +1,28 @@
+# class Solution:
+#     def rearrangeArray(self, nums: List[int]) -> List[int]:
+#         positive=[]
+#         negative=[]
+#         for num in nums:
+#             if num>0:
+#                 positive.append(num)
+#             else:
+#                 negative.append(num)
+#         print(positive , negative)
+#         new_list=[]
+#         for i in range(len(positive)):
+#             new_list.append(positive[i])
+#             new_list.append(negative[i])
+#         return new_list
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        positive=[]
-        negative=[]
+        pnumber=0
+        nnumber=1
+        answer=[0]*len(nums)
         for num in nums:
             if num>0:
-                positive.append(num)
+                answer[pnumber]=num
+                pnumber+=2
             else:
-                negative.append(num)
-        print(positive , negative)
-        new_list=[]
-        for i in range(len(positive)):
-            new_list.append(positive[i])
-            new_list.append(negative[i])
-        return new_list
+                answer[nnumber]=num
+                nnumber+=2
+        return answer
