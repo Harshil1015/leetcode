9255e7bee84d47e726873e8b02bc1ddefc800cdd
Harshil1015 <harshil9225@gmail.com>
2025-07-08 12:32:52 -0400

    118. Pascal's Triangle

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -1,15 +1,14 @@
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        if numRows == 1:
-            return [[1]]
-        
-        prevRows = self.generate(numRows - 1)
-        newRow = [1] * numRows
-        
-        for i in range(1, numRows - 1):
-            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
-        
-        prevRows.append(newRow)
-        return prevRows
+class Solution(object):
+    def generate(self, numRows):
+        def ncr(n,r):
+            result=1
+            for i in range(r):
+                result=result*(n-i)//(i+1)
+            return result
+        answer=[]
+        for n in range(numRows):
+            row=[]
+            for r in range(n+1):
+                row.append(ncr(n,r))
+            answer.append(row)
+        return answer
