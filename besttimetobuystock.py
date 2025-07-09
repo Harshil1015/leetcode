9e37645611d5571ea6d9fd7bad62e769ecfd4776
Harshil1015 <165977497+Harshil1015@users.noreply.github.com>
2025-07-09 17:46:33 -0400

    Update besttimetobuystock.py

--- besttimetobuystock.py
+++ besttimetobuystock.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         a=len(prices)
         b=min(prices)
@@ -7,4 +7,16 @@ class Solution:
         for i in range(buy,a):
             if prices[i]>b:
                profit[i]= prices[i]-b
-        return max(profit)
+        return max(profit)'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy_price = prices[0]
+        profit = 0
+
+        for p in prices[1:]:
+            if buy_price > p:
+                buy_price = p
+            
+            profit = max(profit, p - buy_price)
+        
+        return profit
