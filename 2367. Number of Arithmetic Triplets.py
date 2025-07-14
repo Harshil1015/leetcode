47c2c5f16d8679e610347a130a8673aaee4b4c19
Harshil1015 <harshil9225@gmail.com>
2025-07-13 22:46:31 -0400

    best case of problem

--- 2367. Number of Arithmetic Triplets.py
+++ 2367. Number of Arithmetic Triplets.py
@@ -1,10 +1,17 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        # count = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         if nums[j] - nums[i] == diff:
+        #             if (nums[j] + diff) in nums:
+        #                 count += 1
+        # return count
+        num_set = set(nums)
         count = 0
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[j] - nums[i] == diff:
-                    if (nums[j] + diff) in nums:
-                        count += 1
+
+        for i in range( len (nums)):
+            if (nums[i] + diff) in num_set and (nums[i]+ 2*diff) in num_set:
+                count+=1
         return count
